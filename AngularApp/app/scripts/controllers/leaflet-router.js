2a918a1b85cb695e6cc0e8e3052d3f0e1f53b8d4
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-03-11 12:26:32 -0700

    map view as entire page is AngularApp/app/leaflet-router.html

---
+++ AngularApp/app/scripts/controllers/leaflet-router.js
@@ -0,0 +1,74 @@
+/**
+ * Created by Nicholas Hallahan <nhallahan@spatialdev.com>
+ *     on Tue Mar 11 2014
+ */
+
+angular.module( "leaflet-router", ["leaflet-directive"]).controller("LeafletRouterController", [
+                '$scope', '$http', 'leafletData', '$location',
+                function($scope, $http, leafletData, $location) {
+
+    var map = leafletData.getMap();
+
+    var path = $location.path();
+    console.log(path);
+
+    var coords = path.slice(path.indexOf('@')+1,path.indexOf('z')).split(',');
+
+    var lat = parseFloat(coords[0]) || 11.243062;
+    var lng = parseFloat(coords[1]) || 123.090820;
+    var zoom = parseInt(coords[2]) || 6;
+    console.log(lat);
+    console.log(lng);
+    console.log(zoom);
+
+    var layers = path.slice(path.indexOf('(')+1,path.indexOf(')')).split(',');
+    var basemap = layers[0] || 'http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}';
+    layers = layers.slice(1);
+
+    var layerStr = '';
+    if (layers.length > 0) {
+        layerStr = ',' + layers.toString();
+    }
+    $location.path('/map@'+lat+','+lng+','+zoom+'z('+basemap+layerStr+')');
+
+    angular.extend($scope, {
+        center: {
+            lat: lat,
+            lng: lng,
+            zoom: zoom
+        },
+        defaults: {
+            tileLayer: basemap,
+            scrollWheelZoom: true
+        }
+    });
+
+    leafletData.getMap().then(function(map) {
+        map.on('move',function(){ // moveend is good too
+            var c = map.getCenter();
+            var zoom = map.getZoom();
+            var lat = c.lat.toFixed(6);
+            var lng = c.lng.toFixed(6);
+            $location.path('/map@'+lat+','+lng+','+zoom+'z('+basemap+layerStr+')');
+        });
+    });
+
+    // Get the countries geojson data from a JSON
+    $http.get("data/test/phl.geojson").success(function(data, status) {
+        angular.extend($scope, {
+            geojson: {
+                data: data,
+                style: {
+                    fillColor: "#e8e0d8",
+                    weight: 2,
+                    opacity: 1,
+                    color: 'white',
+                    fillOpacity: 0.8
+                }
+            }
+        });
+    });
+
+}
+
+]);
