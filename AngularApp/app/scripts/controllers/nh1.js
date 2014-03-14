6c5c7cb932458b96017a7465ed29e720589f5e20
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-03-13 17:31:10 -0700

    basic app routing concept understood

---
+++ AngularApp/app/scripts/controllers/nh1.js
@@ -0,0 +1,8 @@
+/**
+ * Created by nick on 3/13/14.
+ */
+
+angular.module('angularAppApp')
+    .controller('NH1Ctrl', function ($scope, $routeParams) {
+        $scope.params = $routeParams;
+    });
