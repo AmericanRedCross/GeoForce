6d03f67731fd084cfe1d52644e7610663a61d177
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-03-13 17:31:10 -0700

    basic app routing concept understood

--- AngularApp/app/scripts/app.js
+++ AngularApp/app/scripts/app.js
@@ -1,18 +1,22 @@
 'use strict';
 
 angular.module('angularAppApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+
+        }).when('/:map*\/stories/:stories', {
+            templateUrl: 'views/nh1.html',
+            controller: 'NH1Ctrl'
+
+        }).otherwise({
+            redirectTo: '/'
+        });
+
+    });
