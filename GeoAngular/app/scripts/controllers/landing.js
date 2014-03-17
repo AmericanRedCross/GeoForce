f835901f40814ce32fdd4b9c0dc107853a9a1549
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-03-17 12:00:15 -0700

    working on routes with views in main app controller

---
+++ GeoAngular/app/scripts/controllers/landing.js
@@ -0,0 +1,14 @@
+(function() {'use strict';
+
+angular.module('GeoAngular').controller('LandingCtrl', LandingCtrl);
+
+
+function LandingCtrl($scope) {
+    $scope.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+    ];
+}
+
+})();
--- GeoAngular/app/scripts/controllers/nh1.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by nick on 3/13/14.
- */
-
-angular.module('GeoAngular')
-    .controller('NH1Ctrl', function ($scope, $routeParams) {
-        $scope.params = $routeParams;
-    });
