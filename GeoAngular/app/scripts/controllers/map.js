e3e0d37112079c3538674ee0d812e095ea131d10
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-03-17 15:16:37 -0700

    angular leaflet map inside of view shows map as per parameters correctly

--- GeoAngular/app/scripts/controllers/map.js
+++ GeoAngular/app/scripts/controllers/map.js
@@ -5,11 +5,31 @@
 
 (function () { 'use strict';
 
-angular.module('GeoAngular', ['leaflet-directive']).controller('MapCtrl', MapCtrl);
+angular.module('GeoAngular').controller('MapCtrl', MapCtrl);
 
 
-function MapCtrl(leafletData, $scope, $routeParams, $location) {
-    console.log('hi');
+function MapCtrl($scope, $routeParams, leafletData) {
+    $scope.test = 'here';
+    $routeParams.stories = 'inmapctr';
+
+    var lat     = parseFloat($routeParams.lat)   || 0;
+    var lng     = parseFloat($routeParams.lng)   || 0;
+    var zoom    = parseFloat($routeParams.zoom)  || 2;
+    var layers  = $routeParams.layers.split(',') || 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+
+    // first layer should always be treated as the basemap
+    var basemap = layers[0];
+    layers = layers.slice(1);
+
+    $scope.center = {
+        lat: lat,
+        lng: lng,
+        zoom: zoom
+    };
+    $scope.defaults = {
+        tileLayer: basemap,
+        scrollWheelZoom: true
+    }
 
 }
 
