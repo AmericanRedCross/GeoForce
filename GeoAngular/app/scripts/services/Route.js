64060ca26c4a0dcabc4f6a173ac0af243bb932db
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-03-19 13:29:38 -0700

    mondo mega refactor of angular app. Nick is learning.

--- GeoAngular/app/scripts/services/Route.js
+++ GeoAngular/app/scripts/services/Route.js
@@ -3,35 +3,63 @@
  *     on Mon Mar 17 2014
  */
 
-(function () { 'use strict';
+angular.module('GeoAngular').factory('Route', function ($rootScope, $location) {
 
-angular.module('GeoAngular').service('Route', Route);
+  // default route parameters. gets reset by AppCtrl.
+  var params = {
+    lat: '0',
+    lng: '0',
+    zoom: '2',
+    layers: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    stories: 'Route' //NH TODO take out, just for debugging...
+  };
 
-function Route($rootScope) {
-    // default route parameters. gets reset by AppCtrl.
-    this.params = {
-        lat: '0',
-        lng: '0',
-        zoom: '2',
-        layers: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-        stories: 'Route' //NH TODO take out, just for debugging...
-    };
+  function updateLocation() {
+    $location.path( '/map@' + params.lat +
+        ',' + params.lng +
+        ',' + params.zoom +
+        '(' + params.layers +
+        ')/stories/' + params.stories);
+  }
 
-    this.setRouteParams = function(routeParams) {
-        this.params = routeParams;
+  return function(routeParams) {
+    // only do this stuff if we have an arg in the fn call
+    if (typeof routeParams !== 'undefined' && routeParams !== null) {
+      angular.extend(params, routeParams);
+      updateLocation();
+      $rootScope.$broadcast('route-update', params);
     }
+    return params;
+  };
 
-    this.getRouteParams = function() {
-        return this.params;
-    }
 
-    this.updateLatLngZoom = function(lat, lng, zoom) {
-        var params = this.params;
-        params.lat = lat;
-        params.lng = lng;
-        params.zoom = zoom;
-        $rootScope.$broadcast('update-lat-lng-zoom', params);
-    }
-}
+//  {
+//    get: function () {
+//      return params;
+//    },
+//
+//    /**
+//     * This will replace the internal params object completely.
+//     * @param routeParams
+//     */
+//    set: function (routeParams) {
+//      params = routeParams;
+//      updateLocation();
+//      $rootScope.$broadcast('route-update', params);
+//    },
+//
+//    /**
+//     * You can provide a subset or superset of params and
+//     * the object will be updated accordingly.
+//     * @param routeParams
+//     */
+//    update: function (routeParams) {
+//      for (var param in routeParams) {
+//        params[param] = routeParams[param];
+//      }
+//      updateLocation();
+//      $rootScope.$broadcast('route-update', params);
+//    }
+//  };
 
-})();
+});
