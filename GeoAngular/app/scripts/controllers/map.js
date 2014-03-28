1ac500f6050dc62f4e51f9a0210f174501c36c4a
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-03-28 10:54:27 -0700

    cleaned out things im not using in map controller

--- GeoAngular/app/scripts/controllers/map.js
+++ GeoAngular/app/scripts/controllers/map.js
@@ -35,10 +35,6 @@ angular.module('GeoAngular').controller('MapCtrl', function ($scope, leafletData
     var basemap = Alias.find(layers[0]) || Alias.redcross;
     var overlays = layers.slice(1);
 
-//    $scope.layers = {
-//      overlays: {}
-//    };
-
     if (lastLayersStr !== layersStr) {
       console.log('Setting layers.');
       if (Array.isArray(overlays) && overlays.length > 0)
@@ -125,45 +121,7 @@ angular.module('GeoAngular').controller('MapCtrl', function ($scope, leafletData
       }
 
     });
-
-
-
-    // NH TODO: Put this in VectorProvider
-//    leafletData.getMap().then(function (map) {
-//      console.log('Adding Overlays...' + JSON.stringify(overlays));
-//      for (var i = 0, len = overlays.length; i < len; ++i) {
-//        var o = overlays[i];
-//
-//        // KML
-//        if (o.slice(o.length - 3) === 'kml') {
-//          addKml(o);
-//        }
-//
-//      }
-//
-//    });
   }
 
-
-  // NH TODO: Put this stuff in VectorProvider
-  function addKml(url) {
-    omnivore.kml(o).on('ready',function (p) {
-      // when this is fired, the layer
-      // is done being initialized
-      console.log('kml ready');
-      console.log(p);
-
-    }).on('error',function (e) {
-          console.warn('Error loading kml. Trying php proxy...');
-
-          var layer = omnivore.kml('proxy.php?' + o).on('error', function (e) {
-            console.error('giving up loading kml...');
-          })
-
-          layer.addTo(map);
-
-
-        }).addTo(map);
-  }
 
 });
