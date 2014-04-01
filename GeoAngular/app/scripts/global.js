e4cb56505f769cf84a0e4a6e9cdafd656a777753
Ryan Whitley <rwhitley@spatialdev.com>
2014-04-01 12:39:57 -0700

    map resize when panel resizes.

--- GeoAngular/app/scripts/global.js
+++ GeoAngular/app/scripts/global.js
@@ -10,3 +10,18 @@ window.RouteParams = {
   layers: 'https://{s}.tiles.mapbox.com/v3/americanredcross.hcji22de/{z}/{x}/{y}.png'
 };
 
+//set up jQuery layout
+window.layout = $('#AppContainer').layout({
+  applyDefaultStyles: false,
+  west__resizable: false,
+  west__closable: false,
+
+  west__spacing_open: 0,
+  north__spacing_open: 0,
+  center__onresize_end: function(){
+
+  }
+});
+
+//resize left(west panel) to be 59px wide.
+window.layout.sizePane("west", 59);
