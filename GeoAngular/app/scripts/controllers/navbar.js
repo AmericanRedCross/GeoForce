cf329106c122faa87abe2de590fd483dd07579e9
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-04-02 14:28:16 -0700

    toggling of left panel buttons working right

--- GeoAngular/app/scripts/controllers/navbar.js
+++ GeoAngular/app/scripts/controllers/navbar.js
@@ -4,10 +4,16 @@ angular.module('GeoAngular').controller('NavBarCtrl', function($scope, $state, $
 
   $scope.isNotState = function (stateName) {
     return $state.$current.name !== stateName;
-  }
+  };
 
   $scope.isState = function (stateName) {
     return $state.$current.name === stateName;
+  };
+
+  $scope.toggleState = function(stateName) {
+    var state = $state.current.name !== stateName ? stateName : 'main';
+    $state.go(state, $stateParams);
+    $scope['is'+stateName[0].toUpperCase()+stateName.slice(1)+'Active'] = false;
   }
 
 });
