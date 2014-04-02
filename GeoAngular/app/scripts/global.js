7a678aa2f7e274489302984ea699115c54a6b02a
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-04-02 12:28:03 -0700

    remved RouteParams global

--- GeoAngular/app/scripts/global.js
+++ GeoAngular/app/scripts/global.js
@@ -3,13 +3,6 @@
  *       on 3/28/14.
  */
 
-window.RouteParams = {
-  lat: '45',
-  lng: '0',
-  zoom: '4',
-  layers: 'https://{s}.tiles.mapbox.com/v3/americanredcross.hcji22de/{z}/{x}/{y}.png'
-};
-
 //set up jQuery layout
 window.layout = $('#AppContainer').layout({
   applyDefaultStyles: false,
