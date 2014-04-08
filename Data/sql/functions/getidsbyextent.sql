191c93681d73fffeeb6415158c036e3bf6598898
Ryan Whitley <rwhitley@spatialdev.com>
2014-04-08 15:06:50 -0700

    update to getidsbyextent to include arc regions

--- Data/sql/functions/getidsbyextent.sql
+++ Data/sql/functions/getidsbyextent.sql
@@ -1,9 +1,9 @@
 ﻿-- Function: udf_getidsbyextent(integer, character varying)
 
--- DROP FUNCTION udf_getidsbyextent(integer, character varying);
+DROP FUNCTION udf_getidsbyextent(integer, character varying);
 
 CREATE OR REPLACE FUNCTION udf_getidsbyextent(IN ingadm_level integer, IN inwkt character varying)
-  RETURNS TABLE(level integer, guid uuid, name text) AS
+  RETURNS TABLE(level integer, guid character varying, name text) AS
 $BODY$
 DECLARE
 
@@ -23,7 +23,7 @@ BEGIN
 
 		
 	--Return Red Cross Regions if above level 0
-	countarc:= count(g.guid) from gadm0 g WHERE ST_Intersects(wkt, geom_simplify_med) AND year = 2012;
+	countarc:= count(g.gid) from arc_regions_dissolved g WHERE ST_Intersects(wkt, geom);
 	IF(countarc < 3) THEN --start count0 < 3
 
 	--Starting with GADM 0, find if the viewport passed in is completely within any of the boundaries.
@@ -58,27 +58,33 @@ BEGIN
 
 						ELSE --count3 = 1
 							--0 or > 1 features.  Just return those.
-							RETURN QUERY SELECT 3, g.guid, name_3 from gadm3 g where ST_Intersects(wkt, geom_simplify_med) AND year = 2012;
+							RETURN QUERY SELECT 3, g.guid::character varying, name_3 from gadm3 g where ST_Intersects(wkt, geom_simplify_med) AND year = 2012;
 						END IF; --count3 = 1
 
 
 				ELSE --count2 = 1
 					--0 or > 1 features.  Just return those.
-					RETURN QUERY SELECT 2, g.guid, name_2 from gadm2 g where ST_Intersects(wkt, geom_simplify_med) AND year = 2012;
+					RETURN QUERY SELECT 2, g.guid::character varying, name_2 from gadm2 g where ST_Intersects(wkt, geom_simplify_med) AND year = 2012;
 				END IF; --count2 = 1
 
 
 
 			ELSE --count1 = 1
 				--0 or > 1 features.  Just return those.
-				RETURN QUERY SELECT 1, g.guid, name_1 from gadm1 g where ST_Intersects(wkt, geom_simplify_med) AND year = 2012;
+				RETURN QUERY SELECT 1, g.guid::character varying, name_1 from gadm1 g where ST_Intersects(wkt, geom_simplify_med) AND year = 2012;
 			END IF; --count1 = 1
 
 
 	ELSE --count0 <= 2
 		--0 or > 1 features.  Just return those.
-		RETURN QUERY SELECT 0, g.guid, name_0 from gadm0 g where ST_Intersects(wkt, geom_simplify_med) AND year = 2012;
+		RETURN QUERY SELECT 0, g.guid::character varying, name_0 from gadm0 g where ST_Intersects(wkt, geom_simplify_med) AND year = 2012;
 	END IF; --count0 <= 2
+
+
+	ELSE
+	--Show ARC Regions (-1 corresponds to ARC Regions)
+		RETURN QUERY SELECT -1, g.gid::character varying, arcregion2::text from arc_regions_dissolved g where ST_Intersects(wkt, geom);
+	END IF;
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
