f3a53b0ab64dd8de84afd3d0103d2fa500bd039e
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-04-07 20:46:42 -0700

    Label class for putting labels on the map

---
+++ GeoAngular/app/scripts/featurelabel/Label.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Nicholas Hallahan <nhallahan@spatialdev.com>
+ *       on 4/7/14.
+ */
+
+(function () {
+  'use strict';
+
+  L.spatialdev.featurelabel.Label = function(point, text) {
+    console.log('LABEL: ' + text + ' (' + point.x + ', ' + point.y + ')');
+  };
+
+}());
