ccec87f0a2ac86c897ab2fe4981cd4754574f8fc
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-04-14 10:40:43 -0700

    added gadm dissolve sql as well as PDFs from DC Meeting.

---
+++ Data/sql/gadm_dissolveSQL/gadm2_Dissolve.sql
@@ -0,0 +1,27 @@
+﻿CREATE Table Burundi_gadm0 AS
+SELECT id_0, name_0, ST_Union(geom) as the_geom
+FROM gadm2
+WHERE name_0 = 'Burundi'
+GROUP BY id_0, name_0;
+
+
+CREATE Table Burundi_gadm1 AS
+SELECT id_1, name_1,id_0, name_0, ST_Union(geom) as the_geom
+FROM gadm2
+WHERE name_0 = 'Burundi'
+GROUP BY id_1, name_1, id_0, name_0;
+
+
+CREATE Table Burundi_gadm2 AS
+SELECT id_2, name_2, id_1, name_1,id_0, name_0, ST_Union(geom) as the_geom
+FROM gadm2
+WHERE name_0 = 'Burundi'
+GROUP BY id_2, name_2, id_1, name_1,id_0, name_0;
+
+
+
+
+
+
+
+
---
+++ Data/sql/gadm_dissolveSQL/gadm4_udf_dissolveallcountries.sql
@@ -0,0 +1,21 @@
+﻿CREATE OR REPLACE FUNCTION gadm4_udf_dissolveallcountries() 
+RETURNS integer AS $$
+DECLARE
+    projectrow RECORD;
+
+BEGIN
+
+    FOR projectrow IN SELECT DISTINCT ON (name_0) name_0 FROM gadm2 WHERE name_4 IS NOT NULL AND name_0 not in ('Spain','Canada', 'Estonia', 'Thailand')
+ LOOP
+    
+	
+        -- Now "projectrow" has one record from cs_materialized_views
+        RAISE NOTICE ' %', projectrow.name_0;
+        PERFORM gadm4_udf_dissolve(projectrow.name_0);
+        
+    END LOOP;
+    RETURN 1;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM gadm4_udf_dissolveallcountries();
