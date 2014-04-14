5fd2f96f1fc9777a59cbfaae4674d246a5d4bf50
Ryan Whitley <rwhitley@spatialdev.com>
2014-04-14 10:44:00 -0700

    Updates to succubus, chubbs endpoint, layerconfig

---
+++ Data/sql/functions/add_gadmid_indexes.sql
@@ -0,0 +1,55 @@
+﻿-- Index: idx_gadm0_id_0
+
+-- DROP INDEX idx_gadm0_id_0;
+
+CREATE UNIQUE INDEX idx_gadm0_id_0
+  ON gadm0
+  USING btree
+  (id_0);
+
+
+-- Index: idx_gadm1_id_0
+
+-- DROP INDEX idx_gadm1_id_0;
+
+CREATE INDEX idx_gadm1_id_0
+  ON gadm1
+  USING btree
+  (id_0);
+
+-- Index: idx_gadm1_id_1
+
+-- DROP INDEX idx_gadm1_id_1;
+
+CREATE INDEX idx_gadm1_id_1
+  ON gadm1
+  USING btree
+  (id_1);
+
+-- Index: idx_gadm2_id_0
+
+-- DROP INDEX idx_gadm2_id_0;
+
+CREATE INDEX idx_gadm2_id_0
+  ON gadm2
+  USING btree
+  (id_0);
+
+-- Index: idx_gadm2_id_1
+
+-- DROP INDEX idx_gadm2_id_1;
+
+CREATE INDEX idx_gadm2_id_1
+  ON gadm2
+  USING btree
+  (id_1);
+
+
+-- Index: idx_gadm2_id_2
+
+-- DROP INDEX idx_gadm2_id_2;
+
+CREATE INDEX idx_gadm2_id_2
+  ON gadm2
+  USING btree
+  (id_2);
