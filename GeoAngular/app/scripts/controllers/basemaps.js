49ca9065358c24910b2475fd6bc3266f2ccda573
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-04-16 14:28:47 -0700

    switching basemaps working

--- GeoAngular/app/scripts/controllers/basemaps.js
+++ GeoAngular/app/scripts/controllers/basemaps.js
@@ -3,8 +3,35 @@
  *       on 3/28/14.
  */
 
-angular.module('GeoAngular').controller('BasemapsCtrl', function($scope) {
+angular.module('GeoAngular').controller('BasemapsCtrl', function($scope, $state, $stateParams) {
   console.log('BasemapsCtrl');
-  $scope.params = $stateParams;
+
+  $scope.getLayers = function() {
+    if (! $stateParams || !$stateParams.layers) {
+      return null;
+    }
+    var layers = $stateParams.layers.split(',');
+    return layers;
+  };
+
+  $scope.currentBasemap = function () {
+    var layers = $scope.getLayers();
+    if (!layers) return null;
+    return layers[0];
+  };
+
+  $scope.swapBasemap = function (basemap) {
+    var layers = $scope.getLayers();
+    layers[0] = basemap;
+    layers = layers.join(',');
+    angular.extend($stateParams, {
+      layers: layers
+    });
+    $state.go('main', $stateParams);
+  };
+
+  $scope.isActiveBasemap = function(basemap) {
+    return $scope.currentBasemap() === basemap;
+  };
 
 });
