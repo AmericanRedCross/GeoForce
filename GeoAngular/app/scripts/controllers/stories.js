ca022dcbec1c44a29672547a4a31077f5cd0e586
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-04-16 12:50:42 -0700

    sideview resizeable working

--- GeoAngular/app/scripts/controllers/stories.js
+++ GeoAngular/app/scripts/controllers/stories.js
@@ -7,15 +7,4 @@ angular.module('GeoAngular').controller('StoriesCtrl', function($scope, $statePa
   console.log('StoriesCtrl');
   $scope.params = $stateParams;
 
-  //resize function
-  $scope.resizeStoriesScrollablePanel = function(){
-    $(".container.stories-container .scrollable").height($(".container.stories-container").height() - $(".container.stories-container .scrollable").position().top);  //This should respond to window.resize events and will be set during that event to make sure the panel stays
-  }
-
-  //Connect the layout onresize end event
-  window.layout.panes.center.bind("layoutpaneonresize_end", $scope.resizeStoriesScrollablePanel);
-
-  //For Init.
-  $scope.resizeStoriesScrollablePanel();
-
 });
