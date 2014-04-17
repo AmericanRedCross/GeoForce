5dcd7f9ff23312039930bebf25f848425b191656
Ryan Whitley <rwhitley@spatialdev.com>
2014-04-17 11:00:07 -0700

    Removing unused attempts at breadcrumb updating.

--- GeoAngular/app/scripts/controllers/breadcrumbs.js
+++ GeoAngular/app/scripts/controllers/breadcrumbs.js
@@ -10,42 +10,71 @@ angular.module('GeoAngular').controller('BreadcrumbsCtrl', function($scope, $roo
 
 	var fullStackURL = config.chubbsPath('services/getAdminStack?format=json&adminlevel=:adminlevel&stackid=:guid&datasource=gadm');
 
+	var breadCrumbFeatures = {}; //Store results of the stack lookup here so we can reuse.
+
 	$scope.$on('center-feature', function (event, featObj) {
-		//Using the Center Feature's GUID, fetch the full stack details.
-		var url = fullStackURL.replace(':adminlevel', featObj.level).replace(':guid', featObj.guid);
-		var self = this;
 
-		// not cached because we only fetch when we dont have the feature in the hash
-		$http.get(url).success(function (geojson, status) {
+		//See if the feature stack has already been stored
+		if(breadCrumbFeatures[featObj.level] && breadCrumbFeatures[featObj.level][featObj.guid]){
+			//Already have it
+			updateBreadCrumbs(featObj);
+		}
+		else{
+			//Go fetch it
+			//Using the Center Feature's GUID, fetch the full stack details.
+			var url = fullStackURL.replace(':adminlevel', featObj.level).replace(':guid', featObj.guid);
+			var self = this;
 
-			if (geojson.error) {
-				console.error('Unable to fetch feature: ' + geojson.error);
-				return;
-			}
+			// Fetch from the server only if we don't have it in the hash
+			$http.get(url).success(function (geojson, status) {
 
-			if (!geojson.features || geojson.features.length < 1) {
-				return;
-			}
+				if (geojson.error) {
+					console.error('Unable to fetch feature: ' + geojson.error);
+					return;
+				}
 
-			//Update The Breadcrumb display
-			$scope.level = featObj.level;
-			var feature = geojson.features[0];
-
-			//Clear lower level scope items in case we've zoomed out
-			for(var x=featObj.level; x <= 5; x++){
-				//null out any level values that are LOWER than what we're currently looking at
-				$scope["level" + x] = "";
-			}
-
-			//using the requested level, dig out the properties for that level and ones above it
-			for(var i=featObj.level; i >= 0; i--){
-				$scope["level" + i] = feature.properties["adm" + i + "_name"];
-			}
+				if (!geojson.features || geojson.features.length < 1) {
+					return;
+				}
 
 
+				var featObj = geojson.features[0].properties;
 
-		});
+				//Update
+				updateBreadCrumbs(featObj);
+
+				//Store in the hash.
+				if(breadCrumbFeatures[featObj.level]){
+					breadCrumbFeatures[featObj.level][featObj.guid] = featObj;
+				}
+				else{
+					breadCrumbFeatures[featObj.level] = {};
+					breadCrumbFeatures[featObj.level][featObj.guid] = featObj;
+				}
+
+
+			});
+		}
+
+
+
 
 	});
+
+	function updateBreadCrumbs(featObj) {
+		//Update The Breadcrumb display
+		$scope.level = featObj.level;
+
+		//Clear lower level scope items in case we've zoomed out
+		for (var x = featObj.level; x <= 5; x++) {
+			//null out any level values that are LOWER than what we're currently looking at
+			$scope["level" + x] = "";
+		}
+
+		//using the requested level, dig out the properties for that level and ones above it
+		for (var i = featObj.level; i >= 0; i--) {
+			$scope["level" + i] = featObj["adm" + i + "_name"];
+		}
+	}
 
 });
