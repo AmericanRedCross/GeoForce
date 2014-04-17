cd2d8098d5ff39ecf5f34d2b4b627671a338f981
Ryan Whitley <rwhitley@spatialdev.com>
2014-04-17 15:38:06 -0700

    Updating breadcrumbs.

--- Data/sql/runonce/create_gadm_stacked_guid_table.sql
+++ Data/sql/runonce/create_gadm_stacked_guid_table.sql
@@ -5,15 +5,6 @@ INTO sf_gadm_guids
 FROM gadm0,gadm1,gadm2
 WHERE   gadm0.id_0 = gadm1.id_0 AND gadm1.id_1 = gadm2.id_1 AND gadm2.id_0 = gadm1.id_0;
 
-INSERT INTO sf_gadm_guids( 
-SELECT ''::character varying as nameARC, 0 as guidARC, null::geometry as geomarc, gadm0.name_0 as name, gadm0.guid, gadm0.geom_simplify_med as geom0, gadm1.name_1 as name, gadm1.guid, gadm1.geom_simplify_med as geom1, '' as name2, null as guid2, null::geometry as geom2  
-FROM gadm1, gadm0
-WHERE gadm0.id_0 = gadm1.id_0);
-
-INSERT INTO sf_gadm_guids(
-SELECT ''::character varying as nameARC, 0 as guidARC, null::geometry as geomarc, gadm0.name_0, gadm0.guid, gadm0.geom_simplify_med as geom0,  '' as name1, null as guid1, null::geometry as geom1, '' as name2, null as guid2, null::geometry as geom2  
-FROM gadm0);
-
 --There are no ARCRegions tied to projects in ECOS.  This is just theoretical
 --INSERT INTO sf_aggregated_gadm_projects_counts(
 --SELECT  a.arcregion2 as nameARC, a.gid as guidARC, a.geom as geomARC, 0 as count0, '' as name0, null::geometry as geom0,  0 as count1, '' as name1, null as guid1, null::geometry as geom1, 0 as count2, '' as name2, null as guid2, null::geometry as geom2  
