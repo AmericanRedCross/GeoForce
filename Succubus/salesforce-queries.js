ce6b7439f29e8a9619927267e292d9328d6af26c
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-04-22 18:22:25 -0700

    figured out queries to satisfy disasters theme except for Sum Total by Type of Supplies.

--- Succubus/salesforce-queries.js
+++ Succubus/salesforce-queries.js
@@ -6,49 +6,132 @@
  *     description is not accurate, I would appreciate improvement.
  */
 
+var stub =
+", " +
+", " +
+", " +
+", " +
+", " +
+", " +
+", " +
+", " +
+", " +
+", " +
+", " +
+", " +
+", " +
+", " +
+", " +
+", " +
+"";
+
+var metaFields =
+"Name, " +
+"Id, " +
+"LastModifiedDate, " +
+"SystemModstamp, " +
+"CreatedDate";
+
+var locationFields =
+"Location__r.Admin_0__c, " +
+"Location__r.Admin_1__c, " +
+"Location__r.Admin_2__c, " +
+"Location__r.Admin_3__c, " +
+"Location__r.Admin_4__c, " +
+"Location__r.Admin_5__c, " +
+"Location__r.Custom_Name__c, " +
+"Location__r.GIS_GEO_ID__c, " +
+"Location__r.Internal_Region__c, " +
+"Location__r.ISD_Region__c, " +
+"Location__r.Latitude__c, " +
+"Location__r.Longitude__c, " +
+"Location__r.Type__c";
+
+var disasterFields =
+"Deaths__c, " +
+"People_Affected__c, " +
+"People_Injured__c, " +
+"People_Missing__c, " +
+"Affected_As_Of__c, " +
+"ARC_Region__c, " +
+"Category__c, " +
+"Deaths_As_Of__c, " +
+"Date__c, " +
+"Disaster_Type__c, " +
+"GLIDE_Number__c, " +
+"Injured_As_Of__c, " +
+"IROC_Status__c, " +
+"Is_Test_Data__c, " +
+"Missing_As_Of__c, " +
+"Source_Deaths__c, " +
+"Source_People_Affected__c, " +
+"Source_People_Injured__c, " +
+"Source_People_Missing__c, " +
+"Summary__c, " +
+"Total_ARC_Contributions__c, " +
+"Unique_Disaster_Id__c";
+
+
+var requestForAssistanceFields =
+"Appeal_Source__c, " +
+"Appeal_Type__c, " +
+"ARC_Response_Date__c, " +
+"Beneficiaries_Reached_as_of__c, " +
+"Beneficiaries_Reached_Households__c, " +
+"Beneficiaries_Reached_Individuals__c, " +
+"Beneficiaries_Targeted_Households__c, " +
+"Beneficiaries_Targeted_Individuals__c, " +
+"Date_Issued__c, " +
+"End_Date__c, " +
+"Exchange_Rate__c, " +
+"Future_Contribution__c, " +
+"Requested_Amount__c, " +
+"Requested_Amount_Foreign_Currerncy__c, " +
+"Requested_Amount_USD__c, " +
+"Response_Strategy_Comments__c, " +
+"Start_Date__c, " +
+"Status__c, " +
+"Subject__c, " +
+"Target_Population__c, " +
+"Total_ARC_Contributions__c";
+
+
 module.exports = {
 
-    /**
-     * Selects all of the fields from every project.
-     */
-    allProjects: "Select Id, LastModifiedDate, SystemModstamp, CreatedDate, Target_Beneficiaries_Indirect__c, Target_Beneficiaries_Direct__c, Summary__c, Sub_Sector__c, Start_Date__c, Sector__c, Phase_Name__c, Name, Location__r.Admin_2__c, Location__r.Admin_1__c, Location__r.Admin_0__c, ISD_Region__c, End_Date__c, Location__r.GIS_GEO_ID__c From Project__c",
+  /**
+   * Selects all of the fields from every project.
+   */
+  project: "Select " + metaFields + ", Target_Beneficiaries_Indirect__c, Target_Beneficiaries_Direct__c, Summary__c, Sub_Sector__c, Start_Date__c, End_Date__c, Sector__c, Phase_Name__c, " + locationFields + ", ISD_Region__c From Project__c",
 
-    /**
-     * Selects all of the fields from the organization (Account table).
-     */
-    //allOrganizations: "Select Id, LastModifiedDate, SystemModstamp, CreatedDate, Name, Location__r.Admin_2__c, Location__r.Admin_1__c, Location__r.Admin_0__c, Location__c From Account",
 
-    /**
-     * Shows how many projects there are and the sum of all of the budgets.
-     */
-    //allMoneyInWorld: "Select Count(Name) project_count, Sum(Total_Budget__c) total_budget From Project__c",
+  /**
+   * Shows number of projects and the budget per country.
+   */
+  numProjAndBudgetPerCountry: "Select Count(Name) project_count, Sum(Total_Budget__c) total_budget, Location__r.Admin_0__c From Project__c Group By Location__r.Admin_0__c",
 
-    /**
-     *
-     */
-    numProjAndBudgetPerCountry: "Select Count(Name) project_count, Sum(Total_Budget__c) total_budget, Location__r.Admin_0__c From Project__c Group By Location__r.Admin_0__c",
+  /**
+   * Shows all of the projects grouped by country
+   */
+  projGroupedByCountry: "Select Location__r.Admin_0__c, Location__r.GIS_GEO_ID__c, Count(Name) project_count, Sum(Total_Budget__c) total_budget From Project__c Group By Location__r.Admin_0__c, Location__r.GIS_GEO_ID__c",
 
-    /**
-     * These commented out queries do not work. Please do not submit queries that do not
-     * successfully return results and write a corresponding JSON file. I left these in
-     * so you can uncomment one and see what happens when a query fails.
-     */
-//    allStandardIndicators: "Select Location__r.Admin_0__c, Location__r.Admin_1__c, Location__r.Admin_2__c, Name, Type__c, Collection_Frequency__c, Project__r.Name, Organization__r.Name, Standard__r.Is_Standard__c, Standard__c, Project__c, Organization__c, Location__c From Indicator__c Where Standard__r.Is_Standard__c = true",
-//    year2013TargetsAndTimeframes: "Select Indicator__r.Location__r.Admin_0__c, Indicator__r.Location__r.Admin_2__c, Indicator__r.Location__r.Admin_1__c, Indicator__r.Name, Indicator__r.Collection_Frequency__c, Effective_Date__c, Collection_Period__c, Target__c, Actual__c, Variance__c, Indicator__r.Location__c, Indicator__c From Indicator_Value__c Where FISCAL_YEAR(Effective_Date__c) = 2013",
-//    all4thQuarters: "Select  Indicator__r.Location__r.Admin_0__c, Indicator__r.Location__r.Admin_2__c, Indicator__r.Location__r.Admin_1__c, Indicator__r.Name, Indicator__r.Collection_Frequency__c, Effective_Date__c, Collection_Period__c, Target__c, Actual__c, Variance__c, Indicator__r.Location__c,  Indicator__c From Indicator_Value__c Where FISCAL_QUARTER(Effective_Date__c) = 4",
 
-    /**
-     * Shows all of the projects grouped by country
-     */
-    projGroupedByCountry: "Select Location__r.Admin_0__c, Location__r.GIS_GEO_ID__c, Count(Name) project_count, Sum(Total_Budget__c) total_budget From Project__c Group By Location__r.Admin_0__c, Location__r.GIS_GEO_ID__c"
+  /**
+   * Selects all applicable information relating to the disasters table.
+   */
+  disaster: "Select " + metaFields + ", " + disasterFields + " From Disaster__c",
 
-    /**
-     * Shows all of the data (fields) for a given project.
-     *
-     * NOTE: Queries like this are not necessary. I need results from every field of an
-     * object or results that are "Group By". Queries on individual keys are not what I need.
-     */
-    //allDataForAGivenProject: "Select Id, LastModifiedDate, SystemModstamp, CreatedDate, Indicator__r.Project__r.Start_Date__c, Indicator__r.Project__r.RecordType.Name, Indicator__r.Project__r.RecordTypeId, Indicator__r.Project__r.Program__c, Indicator__r.Project__r.Phase_Name__c, Indicator__r.Project__r.End_Date__c, Indicator__r.Logframe_Element__r.Name, Indicator__r.Logframe_Element__r.Location__c, Indicator__r.Logframe_Element__r.Start_Date__c, Indicator__r.Logframe_Element__r.End_Date__c, Indicator__r.Logframe_Element__r.Custom_Summary__c, Indicator__r.Logframe_Element__r.Type__c, Indicator__r.Logframe_Element__r.Parent__c, Indicator__r.Name, Indicator__r.Custom_Summary__c, Indicator__r.Collection_Frequency__c, Indicator__r.Logframe_Element__c, Indicator__r.Type__c, Indicator__r.Location__c, Indicator__r.Description__c, Target__c, Actual__c, Effective_Date__c, Collection_Period__c, Period__c, Subjective__c, Target_Percent__c, Variance__c, Indicator__c From Indicator_Value__c Where Indicator__r.Project__c = 'a00K0000002F8ov'",
-    //allDataForAGivenProgram: "Select Id, LastModifiedDate, SystemModstamp, CreatedDate, Indicator__r.Project__r.Start_Date__c, Indicator__r.Project__r.RecordType.Name, Indicator__r.Project__r.RecordTypeId, Indicator__r.Project__r.Program__c, Indicator__r.Project__r.Phase_Name__c, Indicator__r.Project__r.End_Date__c, Indicator__r.Logframe_Element__r.Name, Indicator__r.Logframe_Element__r.Location__c, Indicator__r.Logframe_Element__r.Start_Date__c, Indicator__r.Logframe_Element__r.End_Date__c, Indicator__r.Logframe_Element__r.Custom_Summary__c, Indicator__r.Logframe_Element__r.Type__c, Indicator__r.Logframe_Element__r.Parent__c, Indicator__r.Name, Indicator__r.Custom_Summary__c, Indicator__r.Collection_Frequency__c, Indicator__r.Logframe_Element__c, Indicator__r.Type__c, Indicator__r.Location__c, Indicator__r.Description__c, Target__c, Actual__c, Effective_Date__c, Collection_Period__c, Period__c, Subjective__c, Target_Percent__c, Variance__c, Indicator__c From Indicator_Value__c Where Indicator__r.Project__c = 'a00K0000002IZP6' Or Indicator__r.Project__r.Program__c = 'a00K0000002IZP6'"
+
+  /**
+   * Selects the DisasterLocation table so we can do the many to many join between disasters and locations.
+   * A given disaster may have many disaster locations. You may join by Disaster__r.Id
+   */
+  disasterLocation: "Select " + metaFields + ", Disaster__r.Id, " + locationFields + " From Disaster_Location__c",
+
+
+  /**
+   * Selects the Request_For_Assistance__c table.
+   * A given disaster may have many requests for assistance. You may join by Disaster__r.Id
+   */
+  requestForAssistance: "Select " + metaFields + ", Disaster__r.Id, " + requestForAssistanceFields + " From Request_For_Assistance__c"
 
 };
