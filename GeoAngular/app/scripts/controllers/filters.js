e49c9c458ac0cd5e19708cc3fdb54523e72ff0bd
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-05-06 18:08:00 -0700

    created panel for custom filters where clause

--- GeoAngular/app/scripts/controllers/filters.js
+++ GeoAngular/app/scripts/controllers/filters.js
@@ -3,10 +3,20 @@
  *       on 3/27/14.
  */
 
-angular.module('GeoAngular').controller('FiltersCtrl', function($scope) {
+angular.module('GeoAngular').controller('FiltersCtrl', function($scope, $http) {
   console.log('FiltersCtrl');
   $scope.params = $stateParams;
 
   $scope.navTab = 'sectors';
+
+  $http.get('services/custom/custom_operation?name=getprojectsectorlist&format=json', {cache: true}).success(function (data, status) {
+    console.log(JSON.stringify(data));
+  }).error(function() {
+    console.error("Unable to fetch project sector list");
+  });
+
+  $scope.customFilter = function () {
+
+  };
 
 });
