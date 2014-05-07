df008ee6552f25727d8ffa22ea2fab1c50245f13
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-05-07 11:40:02 -0700

    sector filters making where clause: does not show the last checked thing

--- GeoAngular/app/scripts/controllers/filters.js
+++ GeoAngular/app/scripts/controllers/filters.js
@@ -9,11 +9,42 @@ angular.module('GeoAngular').controller('FiltersCtrl', function($scope, $http) {
 
   $scope.navTab = 'sectors';
 
+  $scope.sectors = [];
+
   $http.get(config.chubbsPath('services/custom/custom_operation?name=getprojectsectorlist&format=json'), {cache: true}).success(function (data, status) {
-    console.log(JSON.stringify(data));
+    for (var i = 0, len = data.length; i < len; ++i) {
+      $scope.sectors.push({name: data[i], checked: false});
+    }
+    debug.filtersScope = $scope;
   }).error(function() {
     console.error("Unable to fetch project sector list");
   });
+
+  $scope.sectorsFilter = function () {
+    var sectorFilters = [];
+    for (var i = 0, len = $scope.sectors.length; i < len; ++i) {
+      var sector = $scope.sectors[i];
+      if (sector.checked) {
+        sectorFilters.push(sector.name);
+      }
+    }
+    var filteredSector = sectorFilters.pop();
+
+    // no sectors to filter
+    if (!filteredSector) {
+      $scope.whereClause = 'null';
+      return;
+    }
+
+    $scope.whereClause = "sector__c LIKE '%" + filteredSector + "%' ";
+
+    filteredSector = sectorFilters.pop();
+    while (filteredSector) {
+      $scope.whereClause += "OR sector__c LIKE '%" + filteredSector + "%' ";
+      filteredSector = sectorFilters.pop();
+    }
+
+  };
 
   $scope.customFilter = function () {
 
