7a23b2cd41a8282437f9c17bd76a04e2a1684fd9
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-05-21 13:53:57 -0700

    ready for rich to implement d3 donuts

---
+++ GeoAngular/app/scripts/services/Donuts.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Nicholas Hallahan <nhallahan@spatialdev.com>
+ *   and Rich Gwozdz <rgwozdz@spatialdev.com>
+ *       on 5/21/14.
+ */
+
+angular.module('GeoAngular').factory('Donuts', function () {
+
+  /**
+   * Code that is executed only once in the app goes here.
+   */
+
+
+  /**
+   * Define Class Objects and prototypes here.
+   */
+
+
+  /**
+   * Public functions that are accessible elsewhere in the app.
+   * Ex: Donuts.createDetailsDonut(arg1, arg2);
+   */
+  return {
+
+    /**
+     * Creates a D3 Donut that is located in the details panel.
+     */
+    createDetailsDonut: function(/*args*/) {
+      /**
+       * Instantiate object and return obj.
+       */
+    },
+
+    /**
+     * Creates a D3 donut that goes around a label on the map.
+     */
+    createLabelDonut: function(/*args*/) {
+      /**
+       * Instantiate object and return obj.
+       */
+    }
+
+  };
+
+});
