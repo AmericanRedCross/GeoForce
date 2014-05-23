c262851f6e7bf5a450ef17baa1b7f5c1378bbcbf
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-05-23 09:49:40 -0700

    refactored donuts code to be private to Donuts service.

--- GeoAngular/app/scripts/services/Donuts.js
+++ GeoAngular/app/scripts/services/Donuts.js
@@ -7,13 +7,280 @@
 angular.module('GeoAngular').factory('Donuts', function () {
 
   /**
-   * Code that is executed only once in the app goes here.
+   * Creates a D3 Donut that is located in the details panel.
    */
+  function ExpandoDonut(entities, visualizationDictionary, wrapper, labelPosition) {
+
+    var data,
+      tmpDataset,
+      dataset,
+      width,
+      height,
+      radius,
+      color,
+      pie,
+      arc,
+      svg,
+      path,
+      reportingValueArr,
+      reportingValue,
+      labelWrapper;
+
+    data = {};
+
+    wrapper = $(wrapper)[0];
+    $(wrapper).html('');
+    this.wrapper = wrapper;
+    this.labelWrapper = null;
+    this.svg = null;
+
+    if(typeof labelPosition === 'undefined' || (labelPosition !== 'top' && labelPosition !== 'bottom')) {
+      labelPosition = 'top'
+    }
+
+    // Loop through the entities
+    for (var j = 0, jMax = entities.length; j < jMax; j++) {
+
+      // Split the semi-colon delimited string of reporting values
+      if(entities[j]['sector__c'] === null) {
+        reportingValueArr = [-9999];
+      } else {
+        reportingValueArr = entities[j]['sector__c'].toString().split(';');
+      }
+      // Loop thru the reporting values
+      for (var k = 0, kMax = reportingValueArr.length; k < kMax; k++) {
+
+        // this iteration's reporting value
+        reportingValue = reportingValueArr[k];
+
+        // If we have already come across this id before (and started a count of its frequency), increment the count
+        if(data.hasOwnProperty(reportingValue)) {
+          data[reportingValue]['count']++;
+        }
+
+        else if (reportingValue === ''){
+          // Null report id's come through as an empty string because splitting an empty string, creates an array with one empty string
+          //  We're assigning these to a value of -9999
+
+          // Increment the count of -9999
+          if(data.hasOwnProperty('-9999')) {
+            data['-9999']['count']++;
+          }
+          else {
+            // if this is the first null id, create an object property and start the counter
+            data['-9999'] = {
+              'count': 1,
+              'color': visualizationDictionary[-9999].color,
+              'alias': visualizationDictionary[-9999].label
+            };
+          }
+        }
+        else {
+
+          // if this is the first time we see this id, create an object property and start the counter
+          data[reportingValue] = {
+            'count': 1,
+            'color': visualizationDictionary[reportingValue].color,
+            'alias': visualizationDictionary[reportingValue].label
+          };
+        }
+
+      }
+
+    }
+
+    // prep dataset for D3; need a temp dataset to deal with merging of data counts for 'other' category
+    tmpDataset = [];
+    dataset = [];
+
+    // Push properties from object holding the category counts/colors categories into an object array
+    for (var j in data) {
+      tmpDataset.push(data[j]);
+    }
+
+    // Create an object that will merge the count from all classification catergories that we've deemed as 'other''
+    var mergedOther = {
+      'count': 0,
+      'color': visualizationDictionary[-9999].color,
+      'alias': visualizationDictionary[-9999].label
+    };
+
+    // Merge all 'other' objects; we determine which are 'other' by testing to see if its been assigned the 'other' color
+    for (var k = 0, kMax = tmpDataset.length; k < kMax; k++) {
+
+      if(tmpDataset[k].color === visualizationDictionary[-9999].color) {
+        mergedOther.count = mergedOther.count + tmpDataset[k].count;
+      } else {
+        dataset.push(tmpDataset[k]);
+      }
+    }
+
+    // Add the merge objedt to the dataset we will use in donut chart
+    dataset.push(mergedOther);
 
 
-  /**
-   * Define Class Objects and prototypes here.
-   */
+    // Use jQuery to get this cluster markers height and width (set in the CSS)
+    width = $(wrapper).width();
+    height = $(wrapper).height();
+
+    if(height === 0) {
+      height = width;
+    }
+
+    radius =  (Math.min(width, height) / 2) - 10;
+    labelWrapper = $('<div class="clearfix" style="text-align:center; margin: 10px 0;" ></div>').appendTo(wrapper);
+    var labelSwatch = $('<div style="display:inline-block; width: 1em; height: 1em; border-radius: 50%"></div>').appendTo(labelWrapper);
+    var labelText = $('<div style="display:inline-block; padding-left: 10px;margin-top: -.4em;vertical-align: middle;"></div>').appendTo(labelWrapper);
+
+    pie = d3.layout.pie()
+      .sort(null);
+
+    arc = d3.svg.arc()
+      .innerRadius(radius-radius * 0.4)
+      .outerRadius(radius);
+
+    var arcOver = d3.svg.arc()
+      .outerRadius(radius + 10)
+      .innerRadius((radius-radius * 0.4) + 10);
+
+    // Note that we add 'clusterDonut' as a selector
+    svg = d3.select(wrapper).append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    this.path = svg.selectAll("path")
+      .data(function(){
+        var dataObjArr,
+          dataArr,
+          pieData;
+
+        dataObjArr = dataset;
+
+        dataArr = [];
+
+        for (var i = 0, iMax = dataObjArr.length; i < iMax; i++) {
+          dataArr.push(dataObjArr[i]['count']);
+        }
+
+        pieData = pie(dataArr);
+
+        for (var i = 0, iMax = pieData.length; i < iMax; i++) {
+          pieData[i].data = dataObjArr[i];
+        }
+
+        return pieData;
+      })
+      .enter().append("path")
+      .attr("fill", function(d, j) {
+        return d.data.color;
+      })
+      .attr("d", arc)
+      .attr("cursor","pointer")
+      .attr("cursor","pointer")
+      .on("mouseover", function(d, i) {
+
+        // clear previously active chart wedge
+        d3.select(wrapper[0]).selectAll('path').transition()
+          .duration(100)
+          .attr("d", arc)
+          .attr('opacity',1)
+          .attr('stroke-width',1)
+          .attr('stroke','rgba(255,255,255,1)');
+
+        // Make the mouseover wedge active
+        d3.select(this)
+          .transition()
+          .duration(100)
+          .attr("d", arcOver)
+          .attr('opacity',0.8)
+          .attr('stroke-width',2)
+          .attr('stroke','rgba(255,255,255,1)');
+
+        // Make appropriate label
+        $(labelSwatch).css('background-color', d.data.color);
+        $(labelText).html(d.data.alias);
+
+        // Show the label if currently hidden
+        if($(self.caption).css('display') === 'none') {
+          $(labelWrapper).slideToggle();
+        }
+      })
+      //.on("mouseout", function(d,i) {})
+      .each(function(d, i) {
+
+        // on load, we want the largest chart wedge to be activew
+        if(i !== 0  ){
+          return;
+        }
+
+        d3.select(this)
+          .transition()
+          .duration(100)
+          .attr("d", arcOver)
+          .attr('opacity',0.8)
+          .attr('stroke-width',2)
+          .attr('stroke','rgba(255,255,255,1)');
+
+        // make the label
+        $(labelSwatch).css('background-color', d.data.color);
+        $(labelText).html(d.data.alias);
+
+        if($(labelWrapper).css('display') === 'none') {
+          $(labelWrapper).slideToggle();
+        }
+      });
+
+    if(labelPosition === 'top') {
+      labelWrapper.prependTo(wrapper);
+    } else {
+      labelWrapper.appendTo(wrapper);
+    }
+
+    this.labelWrapper = labelWrapper;
+    this.svg = svg;
+  }
+
+
+
+
+  function makeVisualizationDictionary(categoryEntities, opts){
+
+    var options = opts || {};
+
+    options.unassignedColor = options.unassignedColor || '#CCCCCC';
+    options.unassignedLabel = options.unassignedLabel || 'Not Assigned';
+
+    var defaultPalette = d3.scale.category20().range();
+    var dictionary = {};
+
+    for(var i = 0; i < categoryEntities.length; i++) {
+
+      var color = null;
+
+      if(i < defaultPalette.length) {
+        color = defaultPalette[i];
+      } else {
+        var index = i % defaultPalette.length - 1;
+        color = defaultPalette[index];
+      }
+      dictionary[categoryEntities[i].value] = {
+        value: categoryEntities[i].value,
+        label: categoryEntities[i].label,
+        color: color
+      };
+
+    }
+
+    dictionary[-9999] = {
+      value: -9999,
+      label: options.unassignedLabel,
+      color: options.unassignedColor
+    };
+
+    return dictionary;
+  }
 
 
   /**
@@ -22,287 +289,13 @@ angular.module('GeoAngular').factory('Donuts', function () {
    */
   return {
 
-      /* Make a lookup where we can get a color and label for a donut entity*/
-
-      makeVisualizationDictionary : function(categoryEntities, opts){
-
-          var options = opts || {};
-
-          options.unassignedColor = options.unassignedColor || '#CCCCCC';
-          options.unassignedLabel = options.unassignedLabel || 'Not Assigned';
-
-          var defaultPalette = d3.scale.category20().range();
-          var dictionary = {};
-
-          for(var i = 0; i < categoryEntities.length; i++) {
-
-              var color = null;
-
-              if(i < defaultPalette.length) {
-                  color = defaultPalette[i];
-              } else {
-                  var index = i % defaultPalette.length - 1;
-                  color = defaultPalette[index];
-              }
-              dictionary[categoryEntities[i].value] = {
-                  value: categoryEntities[i].value,
-                  label: categoryEntities[i].label,
-                  color: color
-              };
-
-          }
-
-          dictionary[-9999] = {
-              value: -9999,
-              label: options.unassignedLabel,
-              color: options.unassignedColor
-          };
-
-          return dictionary;
-      },
-    /**
-     * Creates a D3 Donut that is located in the details panel.
-     */
-    ExpandoDonut: function(entities, visualizationDictionary, wrapper, labelPosition){
-
-        var data,
-            tmpDataset,
-            dataset,
-            width,
-            height,
-            radius,
-            color,
-            pie,
-            arc,
-            svg,
-            path,
-            reportingValueArr,
-            reportingValue,
-            labelWrapper;
-
-        data = {};
-
-        wrapper = $(wrapper)[0];
-        $(wrapper).html('');
-        this.wrapper = wrapper;
-        this.labelWrapper = null;
-        this.svg = null;
-
-        if(typeof labelPosition === 'undefined' || (labelPosition !== 'top' && labelPosition !== 'bottom')) {
-            labelPosition = 'top'
-        }
-
-        // Loop through the entities
-        for (var j = 0, jMax = entities.length; j < jMax; j++) {
-
-            // Split the semi-colon delimited string of reporting values
-            if(entities[j]['sector__c'] === null) {
-                reportingValueArr = [-9999];
-            } else {
-                reportingValueArr = entities[j]['sector__c'].toString().split(';');
-            }
-            // Loop thru the reporting values
-            for (var k = 0, kMax = reportingValueArr.length; k < kMax; k++) {
-
-                // this iteration's reporting value
-                reportingValue = reportingValueArr[k];
-
-                // If we have already come across this id before (and started a count of its frequency), increment the count
-                if(data.hasOwnProperty(reportingValue)) {
-                    data[reportingValue]['count']++;
-                }
-
-                else if (reportingValue === ''){
-                    // Null report id's come through as an empty string because splitting an empty string, creates an array with one empty string
-                    //  We're assigning these to a value of -9999
-
-                    // Increment the count of -9999
-                    if(data.hasOwnProperty('-9999')) {
-                        data['-9999']['count']++;
-                    }
-                    else {
-                        // if this is the first null id, create an object property and start the counter
-                        data['-9999'] = {
-                            'count': 1,
-                            'color': visualizationDictionary[-9999].color,
-                            'alias': visualizationDictionary[-9999].label
-                        };
-                    }
-                }
-                else {
-
-                    // if this is the first time we see this id, create an object property and start the counter
-                    data[reportingValue] = {
-                        'count': 1,
-                        'color': visualizationDictionary[reportingValue].color,
-                        'alias': visualizationDictionary[reportingValue].label
-                    };
-                }
-
-            }
-
-        }
-
-        // prep dataset for D3; need a temp dataset to deal with merging of data counts for 'other' category
-        tmpDataset = [];
-        dataset = [];
-
-        // Push properties from object holding the category counts/colors categories into an object array
-        for (var j in data) {
-            tmpDataset.push(data[j]);
-        }
-
-        // Create an object that will merge the count from all classification catergories that we've deemed as 'other''
-        var mergedOther = {
-            'count': 0,
-            'color': visualizationDictionary[-9999].color,
-            'alias': visualizationDictionary[-9999].label,
-        };
-
-        // Merge all 'other' objects; we determine which are 'other' by testing to see if its been assigned the 'other' color
-        for (var k = 0, kMax = tmpDataset.length; k < kMax; k++) {
-
-            if(tmpDataset[k].color === visualizationDictionary[-9999].color) {
-                mergedOther.count = mergedOther.count + tmpDataset[k].count;
-            } else {
-                dataset.push(tmpDataset[k]);
-            }
-        }
-
-        // Add the merge objedt to the dataset we will use in donut chart
-        dataset.push(mergedOther);
-
-
-        // Use jQuery to get this cluster markers height and width (set in the CSS)
-        width = $(wrapper).width();
-        height = $(wrapper).height();
-
-        if(height === 0) {
-            height = width;
-        }
-
-        radius =  (Math.min(width, height) / 2) - 10;
-        labelWrapper = $('<div class="clearfix" style="text-align:center; margin: 10px 0;" ></div>').appendTo(wrapper);
-        var labelSwatch = $('<div style="display:inline-block; width: 1em; height: 1em; border-radius: 50%"></div>').appendTo(labelWrapper);
-        var labelText = $('<div style="display:inline-block; padding-left: 10px;margin-top: -.4em;vertical-align: middle;"></div>').appendTo(labelWrapper);
-
-        pie = d3.layout.pie()
-            .sort(null);
-
-        arc = d3.svg.arc()
-            .innerRadius(radius-radius * 0.4)
-            .outerRadius(radius);
-
-        var arcOver = d3.svg.arc()
-            .outerRadius(radius + 10)
-            .innerRadius((radius-radius * 0.4) + 10);
-
-        // Note that we add 'clusterDonut' as a selector
-        svg = d3.select(wrapper).append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-        this.path = svg.selectAll("path")
-            .data(function(){
-                var dataObjArr,
-                    dataArr,
-                    pieData;
-
-                dataObjArr = dataset;
-
-                dataArr = [];
-
-                for (var i = 0, iMax = dataObjArr.length; i < iMax; i++) {
-                    dataArr.push(dataObjArr[i]['count']);
-                }
-
-                pieData = pie(dataArr);
-
-                for (var i = 0, iMax = pieData.length; i < iMax; i++) {
-                    pieData[i].data = dataObjArr[i];
-                }
-
-                return pieData;
-            })
-            .enter().append("path")
-            .attr("fill", function(d, j) {
-                return d.data.color;
-            })
-            .attr("d", arc)
-            .attr("cursor","pointer")
-            .attr("cursor","pointer")
-            .on("mouseover", function(d, i) {
-
-                // clear previously active chart wedge
-                d3.select(wrapper[0]).selectAll('path').transition()
-                    .duration(100)
-                    .attr("d", arc)
-                    .attr('opacity',1)
-                    .attr('stroke-width',1)
-                    .attr('stroke','rgba(255,255,255,1)');
-
-                // Make the mouseover wedge active
-                d3.select(this)
-                    .transition()
-                    .duration(100)
-                    .attr("d", arcOver)
-                    .attr('opacity',0.8)
-                    .attr('stroke-width',2)
-                    .attr('stroke','rgba(255,255,255,1)');
-
-                // Make appropriate label
-                $(labelSwatch).css('background-color', d.data.color);
-                $(labelText).html(d.data.alias);
-
-                // Show the label if currently hidden
-                if($(self.caption).css('display') === 'none') {
-                    $(labelWrapper).slideToggle();
-                }
-            })
-            //.on("mouseout", function(d,i) {})
-            .each(function(d, i) {
-
-                // on load, we want the largest chart wedge to be activew
-                if(i !== 0  ){
-                    return;
-                }
-
-                d3.select(this)
-                    .transition()
-                    .duration(100)
-                    .attr("d", arcOver)
-                    .attr('opacity',0.8)
-                    .attr('stroke-width',2)
-                    .attr('stroke','rgba(255,255,255,1)');
-
-                // make the label
-                $(labelSwatch).css('background-color', d.data.color);
-                $(labelText).html(d.data.alias);
-
-                if($(labelWrapper).css('display') === 'none') {
-                    $(labelWrapper).slideToggle();
-                }
-            });
-
-        if(labelPosition === 'top') {
-            labelWrapper.prependTo(wrapper);
-        } else {
-            labelWrapper.appendTo(wrapper);
-        }
-
-        this.labelWrapper = labelWrapper;
-        this.svg = svg;
-    },
-
     /**
      * Creates a D3 donut that goes around a label on the map.
      */
-    createLabelDonut: function(/*args*/) {
-      /**
-       * Instantiate object and return obj.
-       */
+    createLabelDonut: function(sectors, projects) {
+      var vizDictionary = makeVisualizationDictionary(sectors);
+      var donut = new ExpandoDonut(projects, vizDictionary, '#details-donut', 'bottom');
+      return donut;
     }
 
   };
