5cdcc6cb546903232e0044344237646a01b08427
Ryan Whitley <apollolm@gmail.com>
2014-05-28 15:38:05 -0700

    Serializing User object.

--- Chubbs/endpoints/authentication/index.js
+++ Chubbs/endpoints/authentication/index.js
@@ -17,6 +17,21 @@ var path = require('path'),
 
 exports.passport = function () {
 
+    // Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Salesforce profile is
+//   serialized and deserialized.
+    passport.serializeUser(function(user, done) {
+        done(null, user);
+    });
+
+    passport.deserializeUser(function(obj, done) {
+        done(null, obj);
+    });
+
     passport.use(new ForceDotComStrategy({
         clientID: settings.salesforce.ConsumerKey,
         clientSecret: settings.salesforce.ClientSecret,
