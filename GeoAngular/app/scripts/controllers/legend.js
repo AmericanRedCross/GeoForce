f4608b94c074b5f26785bb072c226da4b1ea1fde
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-06-10 14:08:23 -0700

    list of layers showing up in legend

--- GeoAngular/app/scripts/controllers/legend.js
+++ GeoAngular/app/scripts/controllers/legend.js
@@ -3,10 +3,27 @@
  *       on 3/27/14.
  */
 
-module.exports = angular.module('GeoAngular').controller('LegendCtrl', function($scope) {
+module.exports = angular.module('GeoAngular').controller('LegendCtrl', function($scope, LayerConfig) {
 
   $scope.$on('layers-update', function (evt, layers) {
+    $scope.layers = [];
+    for (var i = layers.length - 1; i >= 1; i--){
+      var l = layers[i];
+      var lcfg = LayerConfig.find(l);
 
+      var name = lcfg.name;
+      if (!name && lcfg.properties && lcfg.properties.title) {
+        name = lcfg.properties.title;
+      } else if (!name) {
+        name = l;
+      }
+
+      var layer = {
+        name: name
+      };
+
+      $scope.layers.push(layer);
+    }
   });
 
 });
