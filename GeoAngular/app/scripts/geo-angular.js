f3f65e5be6bd591602e822962d8c56b24efb1390
Ryan Whitley <apollolm@gmail.com>
2014-06-17 15:41:01 -0700

    Fixing z-index on side-view panel. Added date,currency and number formatting to disaster and project details panel.

--- GeoAngular/app/scripts/geo-angular.js
+++ GeoAngular/app/scripts/geo-angular.js
@@ -1115,7 +1115,7 @@ module.exports = angular.module('GeoAngular').controller('BreadcrumbsCtrl', func
  *       on 4/9/14.
  */
 
-module.exports = angular.module('GeoAngular').controller('DetailsCtrl', function ($scope, $rootScope, $state, $stateParams, $http, Donuts) {
+module.exports = angular.module('GeoAngular').controller('DetailsCtrl', function ($scope, $rootScope, $state, $stateParams, $http, Donuts, $filter) {
 
   $scope.details = {};
 
@@ -1314,12 +1314,70 @@ module.exports = angular.module('GeoAngular').controller('DetailsCtrl', function
     if (typeof idx === 'number') $scope.activeThemeItemIdx = idx;
     if (themeItems) $scope.activeThemeItemsList = themeItems;
     $scope.itemsList = false;
-    $scope.details = item;
+    $scope.details = formatDetails(item);
     if (!$scope.contextualLayer) {
-      $scope.lessDetails = lessDetails(item);
+      $scope.lessDetails = lessDetails(formatDetails(item));
     }
     $scope.resizeDetailsPanel();
   };
+
+  function formatDetails(details) {
+      var formattedDetails = {};
+      if ($stateParams.theme === 'disaster') {
+          for (var key in details) {
+              var formatter = config.disasterDetailsFormatting[key];
+              if (formatter) {
+                  switch(formatter.split(",")[0]){
+                      case "currency":
+                          formattedDetails[key] = $filter('currency')(details[key], (formatter.split(",")[1] || "USD"));
+                          break;
+
+                      case "number":
+                          formattedDetails[key] = $filter('number')(details[key]);
+                          break;
+
+                      case "date":
+                          formattedDetails[key] = $filter('date')(details[key], "yyyy-dd-MM");
+                          break;
+
+                      default:
+                          formattedDetails[key] = details[key];
+                  }
+              }
+              else{
+                  //No formatting
+                  formattedDetails[key] = details[key];
+              }
+          }
+      } else {
+          for (var key in details) {
+              var formatter = config.projectDetailsFormatting[key];
+              if (formatter) {
+                  switch(formatter.split(",")[0]){
+                      case "currency":
+                          formattedDetails[key] = $filter('currency')(details[key], (formatter.split(",")[1] || "USD"));
+                          break;
+
+                      case "number":
+                          formattedDetails[key] = $filter('number')(details[key]);
+                          break;
+
+                      case "date":
+                          formattedDetails[key] = $filter('date')(details[key], "yyyy-dd-MM");
+                          break;
+
+                      default:
+                          formattedDetails[key] = details[key];
+                  }
+              }
+              else{
+                  //No formatting
+                  formattedDetails[key] = details[key];
+              }
+          }
+      }
+      return formattedDetails;
+  }
 
   function lessDetails(details) {
     var lessDetails = {};
@@ -1361,9 +1419,7 @@ module.exports = angular.module('GeoAngular').controller('DetailsCtrl', function
 
   $scope.resizeDetailsPanel = function() {
     var detailsPanelTop = $('#DetailsPanel').offset().top;
-    //var themeSelectorTop = 694;
-    //var height = themeSelectorTop - detailsPanelTop - 150;
-      var height = $('#MapCtrl').height() - 350;
+      var height = $('#MapCtrl').height() - 350; //Magic Number
     $('#DetailsPanel .InnerContainer ').height(height);
   };
 
