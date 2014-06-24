10de4cb90648b7c28fd80e704f4fbe184690f3d8
Ryan Whitley <apollolm@gmail.com>
2014-06-24 11:27:19 -0700

    Changed login.html basemap to OSMHOT. Updated text from lorem ipsum.

--- GeoAngular/app/scripts/login.js
+++ GeoAngular/app/scripts/login.js
@@ -12,24 +12,7 @@ GeoAngular.run(function ($rootScope, $state, $stateParams) {
 });
 
 GeoAngular.config(function ($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider
-    .when('/landing', '/map@35.029996,-89.340820,4(redcross,themecount)/landing')
-    .when('/main', '/map@35.029996,-89.340820,4(redcross,smartgadm)');
 
-  $stateProvider
-    .state('landing', {
-      url: '/map@:lat,:lng,:zoom(*layers)/landing?title&zoom-extent&stories&layers-panel&filters-panel&filters&legend&basemaps&info&theme&details-panel&search-panel',
-      views: {
-        'details': {
-          template: ' ',
-          controller: 'MainCtrl'
-        },
-        'landing': {
-          templateUrl: 'views/landing.html',
-          controller: 'LandingCtrl'
-        }
-      }
-    })
 
 });
 
