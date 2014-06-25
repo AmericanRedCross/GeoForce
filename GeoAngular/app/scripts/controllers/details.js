f86c2f4d580746b0574cb0f2598f81f89b8d3999
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-06-24 20:27:42 -0700

    fixes a weird bug where the theme in the scope is not synced with the theme in the state params thus breaking the labels

--- GeoAngular/app/scripts/controllers/details.js
+++ GeoAngular/app/scripts/controllers/details.js
@@ -272,9 +272,9 @@ module.exports = angular.module('GeoAngular').controller('DetailsCtrl', function
   });
 
   $scope.$on('route-update', function () {
-    if ($scope.alertUserToClick === false) return;
     var sf_id = $stateParams.sf_id;
     $scope.theme = $stateParams.theme;
+    if ($scope.alertUserToClick === false) return;
     if (sf_id && typeof sf_id === 'string') {
       var url = config.chubbsPath('services/custom/custom_operation?name=doecostextsearch&format=json&text=') + sf_id;
       $http.get(url).success(function (result, status) {
