b4c88a080c2d8f6ed5a5739e1ca01666ac397069
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-06-30 13:33:11 -0700

    2 space indent

--- GeoAngular/app/scripts/config.js
+++ GeoAngular/app/scripts/config.js
@@ -5,11 +5,11 @@
 
 var config = {};
 
-config.chubbsPath = function(relPath) {
-  return window.location.protocol + "//"  + window.location.host + "/" + relPath;
+config.chubbsPath = function (relPath) {
+  return window.location.protocol + "//" + window.location.host + "/" + relPath;
 };
 
-config.proxyPath = function(path) {
+config.proxyPath = function (path) {
   return config.chubbsPath('services/utilities/proxy?url=' + path);
 };
 
@@ -49,25 +49,24 @@ config.disasterDetailsShortList = {
 };
 
 
-
 /**
  * This defines the type of formatting used for each field in the details panel
  *
  * @type {{}}
  */
 config.projectDetailsFormatting = {
-    createddate: "date",
-    systemmodstamp: "date",
-    lastmodifieddate: "date",
-    concept_start_date__c: "date",
-    project_close_out_date__c: "date",
-    end_date__c: "date",
-    start_date__c: "date",
-    proposal_approved_on__c: "date",
-    proposal_submitted_on__c: "date",
-    target_beneficiaries_direct__c: "number",
-    target_beneficiaries_indirect__c: "number",
-    total_budget__c: "currency,$"
+  createddate: "date",
+  systemmodstamp: "date",
+  lastmodifieddate: "date",
+  concept_start_date__c: "date",
+  project_close_out_date__c: "date",
+  end_date__c: "date",
+  start_date__c: "date",
+  proposal_approved_on__c: "date",
+  proposal_submitted_on__c: "date",
+  target_beneficiaries_direct__c: "number",
+  target_beneficiaries_indirect__c: "number",
+  total_budget__c: "currency,$"
 };
 
 
@@ -77,24 +76,24 @@ config.projectDetailsFormatting = {
  * @type {{}}
  */
 config.disasterDetailsFormatting = {
-    date__c: "date",
-    source_people_affected__c: "number",
-    total_arc_contributions__c: "currency,$", //Should be 'currency' followed by a comma and then an ANGULAR currency specification ("CHF" or "USD" or "$" or etc.)
-    source_people_missing__c: "number",
-    source_people_injured__c: "number",
-    source_people_affected__c: "number",
-    source_deaths__c: "number",
-    missing_as_of__c: "number",
-    injured_as_of__c: "number",
-    deaths_as_of__c: "number",
-    affected_as_of__c: "number",
-    people_missing__c: "number",
-    people_injured__c: "number",
-    people_affected__c: "number",
-    deaths__c: "number",
-    createddate: "date",
-    systemmodstamp: "date",
-    lastmodifieddate: "date"
+  date__c: "date",
+  source_people_affected__c: "number",
+  total_arc_contributions__c: "currency,$", //Should be 'currency' followed by a comma and then an ANGULAR currency specification ("CHF" or "USD" or "$" or etc.)
+  source_people_missing__c: "number",
+  source_people_injured__c: "number",
+  source_people_affected__c: "number",
+  source_deaths__c: "number",
+  missing_as_of__c: "number",
+  injured_as_of__c: "number",
+  deaths_as_of__c: "number",
+  affected_as_of__c: "number",
+  people_missing__c: "number",
+  people_injured__c: "number",
+  people_affected__c: "number",
+  deaths__c: "number",
+  createddate: "date",
+  systemmodstamp: "date",
+  lastmodifieddate: "date"
 };
 
 /**
@@ -103,18 +102,18 @@ config.disasterDetailsFormatting = {
  * @type {{}}
  */
 config.RFADetailsFormatting = {
-    arc_response_date__c: "date",
-    beneficiaries_reached_households__c: "number",
-    requested_amount__c: "currency,$", //Should be 'currency' followed by a comma and then an ANGULAR currency specification ("CHF" or "USD" or "$" or etc.)
-    beneficiaries_reached_individuals__c: "number",
-    beneficiaries_targeted_households__c: "number",
-    beneficiaries_targeted_individuals__c: "number",
-    end_date__c: "date",
-    start_date__c: "date",
-    total_arc_contributions__c: "currency,$",
-    requested_amount_foreign_currerncy__c: "currency,CHF ",
-    requested_amount_usd__c: "currency,$",
-    name: "rfaName"
+  arc_response_date__c: "date",
+  beneficiaries_reached_households__c: "number",
+  requested_amount__c: "currency,$", //Should be 'currency' followed by a comma and then an ANGULAR currency specification ("CHF" or "USD" or "$" or etc.)
+  beneficiaries_reached_individuals__c: "number",
+  beneficiaries_targeted_households__c: "number",
+  beneficiaries_targeted_individuals__c: "number",
+  end_date__c: "date",
+  start_date__c: "date",
+  total_arc_contributions__c: "currency,$",
+  requested_amount_foreign_currerncy__c: "currency,CHF ",
+  requested_amount_usd__c: "currency,$",
+  name: "rfaName"
 };
 
 
@@ -143,36 +142,48 @@ config.unwantedProjectDetails = {
  * @type {{}}
  */
 config.unwantedDisasterDetails = {
-    affected_as_of__c: true,
-    createddate: true,
-    deaths_as_of__c: true,
-    unique_disaster_id__c: true,
-    disaster_id__c: true,
-    disaster__r_id: true,
-    id: true,
-    injured_as_of__c: true,
-    is_test_data__c: true,
-    lastmodifieddate: true,
-    level: true,
-    location__r_custom_name__c: true,
-    location__r_gis_geo_id__c: true,
-    location__r_internal_region__c: true,
-    location__r_isd_region__c: true,
-    location__r_latitude__c: true,
-    location__r_longitude__c: true,
-    location__r_type__c: true,
-    missing_as_of__c: true,
-    source_deaths__c: true,
-    source_people_affected__c: true,
-    source_people_injured__c: true,
-    source_people_missing__c: true,
-    systemmodstamp: true,
-    location__r_admin_0__c: function(item){ return (item ? false : true); }, //If item is something, then return false to show it.  Return true to hide it.
-    location__r_admin_1__c: function(item){ return (item ? false : true); },
-    location__r_admin_2__c: function(item){ return (item ? false : true); },
-    location__r_admin_3__c: function(item){ return (item ? false : true); },
-    location__r_admin_4__c: function(item){ return (item ? false : true); },
-    location__r_admin_5__c: function(item){ return (item ? false : true); }
+  affected_as_of__c: true,
+  createddate: true,
+  deaths_as_of__c: true,
+  unique_disaster_id__c: true,
+  disaster_id__c: true,
+  disaster__r_id: true,
+  id: true,
+  injured_as_of__c: true,
+  is_test_data__c: true,
+  lastmodifieddate: true,
+  level: true,
+  location__r_custom_name__c: true,
+  location__r_gis_geo_id__c: true,
+  location__r_internal_region__c: true,
+  location__r_isd_region__c: true,
+  location__r_latitude__c: true,
+  location__r_longitude__c: true,
+  location__r_type__c: true,
+  missing_as_of__c: true,
+  source_deaths__c: true,
+  source_people_affected__c: true,
+  source_people_injured__c: true,
+  source_people_missing__c: true,
+  systemmodstamp: true,
+  location__r_admin_0__c: function (item) {
+    return (item ? false : true);
+  }, //If item is something, then return false to show it.  Return true to hide it.
+  location__r_admin_1__c: function (item) {
+    return (item ? false : true);
+  },
+  location__r_admin_2__c: function (item) {
+    return (item ? false : true);
+  },
+  location__r_admin_3__c: function (item) {
+    return (item ? false : true);
+  },
+  location__r_admin_4__c: function (item) {
+    return (item ? false : true);
+  },
+  location__r_admin_5__c: function (item) {
+    return (item ? false : true);
+  }
 };
 
 /**
@@ -181,17 +192,17 @@ config.unwantedDisasterDetails = {
  * @type {{}}
  */
 config.unwantedRFADetails = {
-    beneficiaries_reached_as_of__c: true,
-    createddate: true,
-    date_issued__c: true,
-    disaster__r_id: true,
-    exchange_rate__c: true,
-    future_contribution__c: true,
-    id: true,
-    lastmodifieddate: true,
-    subject__c: true,
-    target_population__c: true,
-    systemmodstamp: true
+  beneficiaries_reached_as_of__c: true,
+  createddate: true,
+  date_issued__c: true,
+  disaster__r_id: true,
+  exchange_rate__c: true,
+  future_contribution__c: true,
+  id: true,
+  lastmodifieddate: true,
+  subject__c: true,
+  target_population__c: true,
+  systemmodstamp: true
 };
 
 /**
