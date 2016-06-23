03d66910dc02bb27a2783d914b03c72e58c176f3
Daniel Baah <dbaah@spatialdev.com>
2016-06-23 10:25:53 -0700

    update deploy dev script

--- install_dev.sh
+++ install_dev.sh
@@ -1,12 +1,22 @@
-echo !!! INSTALLING MAPFOLIO !!!
+#!/usr/bin/env bash
+echo !!! INSTALLING MAPFOLIO SERVER PRODUCTION !!!
 echo CLONING GIT REPOSITORY FROM GITHUB
 rm -rf GeoForce
 git clone https://github.com/AmericanRedCross/GeoForce.git
-echo COPYING SETTINGS FILES
-cp settings/chubbs_settings_dev.js GeoForce/Chubbs/private/settings.js
-cp settings/succubus_settings_dev.js GeoForce/Succubus/settings.js
+git checkout origin development
+echo COPYING SETTINGS FILES AND SSL CERTIFICATES
+cp settings/chubbs_settings_prod.js GeoForce/Chubbs/private/settings.js
+cp settings/succubus_settings_prod.js GeoForce/Succubus/settings.js
+cp ssl_keys/geo.cer GeoForce/Chubbs/private/geo.cer
+cp ssl_keys/geo.pfx GeoForce/Chubbs/private/geo.pfx
+echo INSTALLING MAPFOLIO DEPENDENCIES
+cd GeoAngular/app/lib/Leaflet.MapboxVectorTile
+npm install
+echo BUILDING geo-angular.js
+cd ../../../
+npm run-script build
 echo INSTALLING CHUBBS DEPENDENCIES
-cd GeoForce/Chubbs
+cd ../Chubbs
 npm install
 echo INSTALLING GEOANGULAR DEPENDENCIES
 cd ../GeoAngular
