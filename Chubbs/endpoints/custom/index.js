44486316ade501eb49202706f08027073f962964
Daniel Baah <dbaah@spatialdev.com>
2017-03-14 23:46:18 -0700

    trim custom searchterm

--- Chubbs/endpoints/custom/index.js
+++ Chubbs/endpoints/custom/index.js
@@ -732,7 +732,7 @@ exports.app = function (passport) {
     function executeCustomLocationAdminNameSearch(searchterm, options, callback){
 
         var query = "SELECT " + (options.returnGeometry === "yes" ? "geom, " : "") + " id, gadm_stack_level, ecos_id, 'Custom' as source, name, country, gadm_stack_guid, level FROM arc_custom_locations WHERE name ILIKE('%' || $1 || '%') ORDER BY name";
-        var sql = {text: query, values: [searchterm]};
+        var sql = {text: query, values: [searchterm.trim()]};
 
         //run it
         common.executePgQuery(sql, callback);
